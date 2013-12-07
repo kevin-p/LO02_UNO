059a5d05ab316216ce986f7211d08527def35813
kevin personnic <personnic.kevin@gmail.com>
2013-12-07 17:49:53 +0100

    ecriture de la méthode choisirCouleur pour un joueur.
    ecriture de la méthode poser pour un joueur.

--- UNOgame/src/com/lo02/UNO/core/Joueur.java
+++ UNOgame/src/com/lo02/UNO/core/Joueur.java
@@ -1,6 +1,9 @@
 package com.lo02.UNO.core;
 
 import com.lo02.UNO.core.cartes.Carte;
+import com.lo02.UNO.core.cartes.Couleur;
+
+import java.io.*;
 
 public class Joueur {
 	
@@ -32,7 +35,10 @@ public class Joueur {
 	}
 	
 	public void poser(int IndexCarte) {
+		Manche manche = Manche.getInstanceManche();
 		
+		if(manche.poserCarte(mainJoueur.get(IndexCarte), this))
+			mainJoueur.remove(IndexCarte);
 	}
 	
 	public void AfficherMain() {
@@ -41,8 +47,28 @@ public class Joueur {
 		}
 	}
 	
-	public void choisirCouleur() {
-		
+	public Couleur choisirCouleur() {
+		//à changer pour fontionner de pair avec l'interface graphique
+		for (;;) {
+			System.out.println(this.nom + " choisissez une couleur (ROUGE/R/r, BLEU/B/b, VERT/V/v, JAUNE/J/j");
+			InputStreamReader lecteur = new InputStreamReader(System.in);
+			BufferedReader entree = new BufferedReader(lecteur);
+			String choix = new String();
+			try {
+				choix = entree.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			if(choix == "ROUGE" || choix == "R" || choix == "r")
+				return Couleur.ROUGE;
+			if(choix == "BlEU" || choix == "B" || choix == "b")
+				return Couleur.BLEU;
+			if(choix == "VERT" || choix == "V" || choix == "v")
+				return Couleur.VERT;
+			if(choix == "JAUNE" || choix == "J" || choix == "j")
+				return Couleur.JAUNE;
+		}
 	}
 	
 	public String getNom() {
