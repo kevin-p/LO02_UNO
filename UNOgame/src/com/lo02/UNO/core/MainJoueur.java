6af7aa2055df24da6b2d7432e9e8b0913a7c6ecb
Kevin <Kevin@Kevin-Ultrabook>
2013-12-11 17:27:11 +0100

    ecriture javadoc de Mainjoueur

--- UNOgame/src/com/lo02/UNO/core/MainJoueur.java
+++ UNOgame/src/com/lo02/UNO/core/MainJoueur.java
@@ -5,7 +5,10 @@ import com.lo02.UNO.core.cartes.Carte;
 
 /**
  * 
+ * Représente la main d'un joueur.
+ * Extends {@link ArrayList} de {@link Carte}
  * 
+ * Stocke aussi des méthode lié à l'organisation de la main d'un joueur
  * 
  * @author Kevin
  *
@@ -17,12 +20,28 @@ public class MainJoueur extends ArrayList<Carte> {
 	 */
 	private static final long serialVersionUID = -7331662739769106381L;
 
+	/**
+	 * 
+	 * Constructeur par defaut
+	 * 
+	 */
 	public MainJoueur () {}
 	
+	
+	/**
+	 * 
+	 * Range les {@link Carte} de la {@link MainJoueur} par ordre croissant
+	 * 
+	 */
 	public void rangerCroiss() {
 		
 	}
 	
+	/**
+	 * 
+	 * Range les {@link Carte} de la {@link MainJoueur} par ordre décroissant
+	 * 
+	 */
 	public void rangerDesc() {
 		
 	}
