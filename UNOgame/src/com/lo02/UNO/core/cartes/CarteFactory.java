0c01050626904a3078a24dd7a6b6ae80250ae67e
Kevin <Kevin@Kevin-Ultrabook>
2013-12-12 02:15:13 +0100

    ecriture de la javadoc de CarteFactory

--- UNOgame/src/com/lo02/UNO/core/cartes/CarteFactory.java
+++ UNOgame/src/com/lo02/UNO/core/cartes/CarteFactory.java
@@ -1,7 +1,29 @@
 package com.lo02.UNO.core.cartes;
 
+/**
+ * 
+ * Foctory de {@link Carte}, permet de construire des objets {@link Carte} ou dérivant de {@link Carte}, comme des 
+ * cartes spéciale facilement.
+ * Les dérivé de {@link Carte} sont construit en fonction du {@link Label} de carte demandé, suivant le type de 
+ * {@link Carte} créé la {@link Couleur} est ignorée.
+ * 
+ * Elle ne possède qu'une méthode static {@link #getCarte(Label, Couleur)} qui renvoi un objet de type {@link Carte}
+ * 
+ * @author Kevin Personnic
+ *
+ */
 public class CarteFactory {
 	
+	/**
+	 * 
+	 * Prend en paramètre un {@link Label} et une {@link Couleur} et renvoi un objet {@link Carte} de UNO adapté.
+	 * Si jamais la {@link Carte} ne possède pas de {@link Couleur} alors quel que soit la {@link Couleur} demandé 
+	 * elle est ignorée.
+	 * 
+	 * @param label {@link Label} le label de la carte à construire
+	 * @param couleur {@link Couleur} la couleur de la carte à construire
+	 * @return carte {@link Carte}
+	 */
 	public static Carte getCarte (Label label, Couleur couleur) {
 		Carte carte;
 		
